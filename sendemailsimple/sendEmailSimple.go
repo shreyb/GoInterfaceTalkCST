e127d321f7dea6f4c1859974898547a3de958075
Shreyas Bhat <sbhat@fnal.gov>
2024-04-10 13:24:39 -0500

    Fixed a couple of dependencies so that the test actually run outside of go present

--- sendemailsimple/sendEmailSimple.go
+++ sendemailsimple/sendEmailSimple.go
@@ -1,36 +1,40 @@
-package main 
+package main
 
 import (
-    "errors"
-    "log"
+	"errors"
+	"log"
 )
 
 // START CUT 1 OMIT
 var errEmptyMessage = errors.New("the message was empty") // HL
 
 func sendEmail(from string, to []string, subj string, msg string) error { // HL
-    if msg == "" {
-        log.Info("Empty message - not sending anything")
-        return errEmptyMessage
-    }
-    err := sendTheEmail(msg)
-    return err // Yes - this could be simply written as return sendTheEmail
-} 
+	if msg == "" {
+		log.Print("Empty message - not sending anything")
+		return errEmptyMessage
+	}
+	err := sendTheEmail(msg)
+	return err // Yes - this could be simply written as return sendTheEmail
+}
+
 // END CUT 1 OMIT
 
 // START CUT 2
 func main() {
-    err := sendEmail( // HL
-        "foo@example.com",
-        []string{"bar@example.com", "baz@example.com"},
-        "[EXTERNAL] - SUPER important",
-        "This is totally not a phishing email.  Click here!"
-    )
-    
-    if err != nil {
-        if !errors.Is(err, errEmptyMessage) {
-            log.Fatal("Couldn't send the message")
-        }
-    }
+	err := sendEmail( // HL
+		"foo@example.com",
+		[]string{"bar@example.com", "baz@example.com"},
+		"[EXTERNAL] - SUPER important",
+		"This is totally not a phishing email.  Click here!",
+	)
+
+	if err != nil {
+		if !errors.Is(err, errEmptyMessage) {
+			log.Fatal("Couldn't send the message")
+		}
+	}
 }
+
 // END CUT 2
+
+func sendTheEmail(msg string) error { return nil }
